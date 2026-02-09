adec911487fc20c036a0a7d2cf2c96e520ef7649
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2026-02-09 15:38:51 +0900

    Update atuin config
    - Disable up-arrow
    - search_mode to fulltext

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -116,7 +116,7 @@ if status is-interactive
     # remove `context` from the left right
     set -U tide_right_prompt_items status cmd_duration jobs direnv bun node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
     if type -q atuin
-        atuin init fish | source
+        atuin init fish --disable-up-arrow | source
     end
     if type -q yazi
         # bind alt+y to yazi
